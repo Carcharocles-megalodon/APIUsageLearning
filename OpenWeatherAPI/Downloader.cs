c61ce7be823936328993e903d81f11737f348d3e
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-04-13 19:29:26 +0100

    Added a basic downloader

---
+++ OpenWeatherAPI/Downloader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OpenWeatherAPI
+{
+    class Downloader
+    {
+        private HttpClient client;
+
+        public Downloader()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<Stream> Get(string uri)
+        {
+            Stream response = await client.GetStreamAsync(uri);
+            response.Position = 0;
+            return response;
+        }
+    }
+}
